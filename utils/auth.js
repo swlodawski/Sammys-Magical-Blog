f880cf83bb3592a1d3066ee9f3422e409d2b365b
sammy wlodawski <samwlodawski@gmail.com>
2024-08-14 14:51:03 -0400

    Added login requirement for content.

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const { Model } = require("sequelize");
+
+const withAuth = (req, res, next) => {
+    if(!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next()
+    }
+};
+
+module.exports = withAuth;
