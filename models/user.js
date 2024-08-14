6544f44b1fd988a4d22d6388bc45530e1018d2fa
sammy wlodawski <samwlodawski@gmail.com>
2024-08-14 16:09:15 -0400

    Added the user model file with brcypt hashing and hooks.

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes} = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+const { update } = require('./blog');
+
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    },
+{
+    hooks: {
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        beforeUpdate: async (updateUserData) => {
+            updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+            return updateUserData;
+        },
+    },
+
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
