5632998477e87e243edff33f6a77e17fdb1626e5
sammy wlodawski <samwlodawski@gmail.com>
2024-08-15 16:48:20 -0400

    Added get single user route and exported router file to app.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -58,6 +58,19 @@ router.get('/', async(req, res) => {
     }
 });
 
+router.get('/:id', async(req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id);
+        if(!userData) {
+            res.status(404).json({message: 'The user with that info does not exist'});
+            return
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
+module.exports = router;
 
 
