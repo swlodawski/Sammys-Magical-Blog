af9a38807a63b7404ca01de56d88c7f60b409fe7
sammy wlodawski <samwlodawski@gmail.com>
2024-08-17 14:20:23 -0400

    Added get route for single user.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -53,4 +53,19 @@ router.get('/', async(req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.get('/:id', async(req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id);
+        if(!userData) {
+            res.status(400).json({message: 'The user does not exist'});
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router;
