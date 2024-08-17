c2c43e00559ef316c6f2e36bd5ecaac8513444fe
sammy wlodawski <samwlodawski@gmail.com>
2024-08-16 21:19:07 -0400

    Finished the post login route for user.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,3 +16,22 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+try {
+    const userData = await User.findOne({ where: {
+        user_name: req.body.user_name
+    }});
+    if (!userData) {
+        res.status(400).json({ message: 'The username or password was entered wrong. Please check again'});
+        return;
+    }
+
+req.session.save(() => {
+    req.session.user_id = userData.id;
+    req.session.logged_in = true;
+});
+} catch (err) {
+    res.status(400).json(err)
+}
+});
+
