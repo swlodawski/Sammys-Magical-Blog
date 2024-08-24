22ad95b29ed13045084f98048e65c2229bc1f278
sammy wlodawski <samwlodawski@gmail.com>
2024-08-24 14:27:30 -0400

    Added post route for user log in.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -33,3 +33,24 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        const {username, password} = req.body;
+
+        if(!username || !password) {
+            res.status(400).json({message: 'The username and password do not match a registered user. Please reenter'});
+    }
+
+        req.session.save(() => {
+        res.session.userId = userData.id;
+        req.session.loggedin = true;
+
+      const {password: _, ...userWithoutPassword} = userData.get({plain: true});
+            res.status(200).json({user: userWithoutPassword, message: 'You are logged in'});
+        });
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router;
