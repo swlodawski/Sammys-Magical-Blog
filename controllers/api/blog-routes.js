ad1eb512b5655a0d8630e5adc20b73f19b0f2098
sammy wlodawski <samwlodawski@gmail.com>
2024-08-24 19:17:46 -0400

    Added blog routes.

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { json } = require('sequelize');
 const {Blog} = require('../../models');
 
 router.get('/home', async (req, res) => {
@@ -9,7 +8,26 @@ router.get('/home', async (req, res) => {
         });
         res.status(200).json(blogData)
     } catch (err) {
-        res.status(500);json(err)
+        res.status(500).json(err)
     }
 });
 
+router.post('/dashboard', async (req, res) => {
+    try {
+        const {title, description} = req.body;
+        if (!title || !description) {
+            return res.status(400).json({message: 'Enter a title and description'});
+        }
+        const blogData = await Blog.create({
+            title,
+            description,
+            userId: req.session.userId
+        });
+        res.status(200).json({post: blogData, message: 'Blog post createds successfully'});  
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+
+module.exports = router;
