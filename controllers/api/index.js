0b827db7dfcf45fae99a214e908256dc710ffde2
sammy wlodawski <samwlodawski@gmail.com>
2024-08-24 13:11:08 -0400

    Adjusted a few file locations and renamed a couple hbs files names.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,76 +1,11 @@
 const router = require('express').Router();
-const {User, Blog} = require('../../models');
 
-router.post('/', async(req, res) => {
-    try {
-        const userData = await User.create(req.body);
+const userRoutes = require('./user-routes');
+const blogRoutes = require('./blog-routes');
 
-        req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
+router.use('/', userRoutes);
+router.use('/', blogRoutes);
 
-        res.status(200).json(userData);
-        });
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
 
-router.post('/login', async(req, res) => {
-    try {
-        const userData = await User.findAll({ where: {
-        user_name:
-        req.body.user_name}});
-
-    if(!userData) {
-        res
-        .status(400).json({ message: 'Please make sure you entered the correct login information'});
-        return;
-    }
-
-    req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-
-        res.json({ user: userData, message: 'You are logged in'});
-    });
-} catch (err) {
-    res.status(400).json(err)
-}
-    });
-
-router.post('/logout', async(req, res) => {
-    if(req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(200).end();
-        });
-    } else {
-        res.status(404).end();
-    }
-});
-
-router.get('/', async(req, res) => {
-    try {
-        const userData = await User.findAll();
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-router.get('/:id', async(req, res) => {
-    try {
-        const userData = await User.findByPk(req.params.id);
-        if(!userData) {
-            res.status(404).json({message: 'The user with that info does not exist'});
-            return
-        }
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(500).json(err)
-    }
-});
 
 module.exports = router;
-
-
