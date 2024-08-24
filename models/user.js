17a28f2b69b2789fbaa9093a59642c285e5b1501
sammy wlodawski <samwlodawski@gmail.com>
2024-08-24 19:33:17 -0400

    Adjusted files names and files.

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const { Model, DataTypes} = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
-const { update } = require('./blog');
 
 class User extends Model {
     checkPassword(loginPW) {
