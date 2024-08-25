43ce646e23a869d6e86289e727cdd4fe66f43c2a
sammy wlodawski <samwlodawski@gmail.com>
2024-08-25 18:35:38 -0400

    Finished auth file.

--- utils/auth.js
+++ utils/auth.js
@@ -16,3 +16,26 @@ const isLoggedOut = async (req, res, next) => {
     }
 };
 
+const loadUser = async (req, res, next) => {
+    if (req.session && req.session.userId) {
+        try {
+            const user = await User.findByPk(req.session.userId);
+            if (user) {
+                req.user = user;
+                res.locals.user = user;
+                res.locals.isLoggedIn = true;
+            } else {
+                res.locals.loggedIn = false;
+            }
+        } catch (err) {
+            console.error('Error logging user:', err);
+            res.locals.isLoggedIn = false;
+        }
+        } else {
+            res.locals.isLoggedIn =false;
+        }
+        next();
+    } 
+
+
+    module.exports = {isLoggedIn, isLoggedOut, loadUser};
