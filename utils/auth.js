aa69dc6134bb431a36c187b72273e99c1f7d6e75
sammy wlodawski <samwlodawski@gmail.com>
2024-08-25 18:10:45 -0400

    Adding auth for loggedin and loggedout status.

--- utils/auth.js
+++ utils/auth.js
@@ -1,2 +1,18 @@
 const User = require('../models/user')
 
+const isLoggedIn = (req, res, next) => {
+    if (req.session && req.session.loggedIn) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
+const isLoggedOut = async (req, res, next) => {
+    if (!req.session || !req.session.loggedIn) {
+        next();
+    } else {
+        req.redirect('/home');
+    }
+};
+
